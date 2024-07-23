6cff0286cd27f50b24ede7f5e5e7113289f167f4
sandro areco <sandro1998.areco@gmail.com>
2024-07-23 19:53:05 -0300

    fin de sección 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-counter',
+    template: `
+    <p>
+        counter: {{ counter }}
+        </p>
+
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="reset()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+    `
+})
+
+export class CounterComponent{
+    public counter: number = 10;
+
+    increaseBy(value: number): void {
+      this.counter += value;
+    }
+  
+    reset():void{
+      this.counter = 10;
+    }
+}
